1f3c37fe988b462e99ecafbbed315e6a8cfefe9d
Yoanna Popova <yoanna.dp@gmail.com>
2024-06-28 10:31:30 +0300

    Create footer component.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,20 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+// import { faSquareFacebook } from '@fortawesome/free-brands-svg-icons';
+
 
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div>
-    {/* <div className='app'>
-        <FontAwesomeIcon icon={faEnvelope} />
-    </div> */}
-
       <Header />
+
       <Home />
+
+      <Footer />
     </div>
   );
 }
