cf741716bdea902af00687d90247f6ca8997f42a
Yoanna Popova <yoanna.dp@gmail.com>
2024-07-09 14:36:59 +0300

    Add login functionality.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+
+import * as authService from './services/authService';
+import AuthContext from './contexts/authContext';
+import Path from './paths';
 
 import Header from './components/header/Header';
 import Home from './components/home/Home';
@@ -9,13 +13,18 @@ import Login from './components/login/Login';
 import AddMovie from './components/add-movie/AddMovie';
 import Footer from './components/footer/Footer';
 import MovieDetails from './components/movie-details/MovieDetails';
-import AuthContext from './contexts/authContext';
 
 function App() {
+  const navigate = useNavigate();
   const [auth, setAuth] = useState({});
 
-  const loginSubmitHandler = (values) => {
-    console.log(values);
+  const loginSubmitHandler = async (values) => {
+    // TRY/CATCH
+    const result = await authService.login(values.email, values.password);
+    setAuth(result);
+    navigate(Path.Home);
+
+    console.log(result);
   };
 
   return (
--- client/src/services/movieService.js
+++ client/src/services/movieService.js
@@ -2,7 +2,6 @@ import * as request from "../lib/request";
 
 const baseUrl = 'http://localhost:3030/jsonstore/movies';
 
-// GET/LOAD ALL AVAILABLE MOVIES
 export const getAll = async () => {
     const result = await request.get(baseUrl);
 
@@ -15,7 +14,6 @@ export const getOne = async (movieId) => {
     return result;
 }
 
-// ADD/CREATE MOVIE
 export const create = async (movieData) => {
     const result = await request.post(baseUrl, movieData);
 
