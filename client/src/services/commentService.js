e2d4d55cb07a58c1ad9c81ed7d458a3ddc36a753
Yoanna Popova <yoanna.dp@gmail.com>
2024-07-10 14:57:43 +0300

    Use server to filter the movie comments.

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -1,16 +1,21 @@
 import * as request from '../lib/request';
 
-const baseUrl = 'http://localhost:3030/jsonstore/comments';
+// const baseUrl = 'http://localhost:3030/jsonstore/comments';
+const baseUrl = 'http://localhost:3030/data/comments';
 
 export const getAll = async (movieId) => {
     const query = new URLSearchParams({
         where: `movieId="${movieId}"`
     });
 
-    const result = await request.get(`${baseUrl}`); // ?${query}
+    const result = await request.get(`${baseUrl}?${query}`); // ?${query}
 
-    // Rewrite this part with query string
-    return Object.values(result).filter(comment => comment.movieId === movieId);
+    // Rewrite this part with query string; temporary solution with jsonstore
+    // return Object.values(result).filter(comment => comment.movieId === movieId);
+
+    // return result.filter(comment => comment.movieId === movieId);
+
+    return result;
 }
 
 export const create = async (movieId, username, text) => {
