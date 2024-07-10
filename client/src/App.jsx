ead6b1cca2d98d1bab54ba5a4546bdb3b57a6d50
Yoanna Popova <yoanna.dp@gmail.com>
2024-07-10 10:51:53 +0300

    Add user register functionality.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,15 +26,19 @@ function App() {
   };
 
   const registerSubmitHandler = async (values) => {
-    console.log(values);
+    // TRY/CATCH
+    // Confirm the two passwords
+    const result = await authService.register(values.email, values.password);
+    setAuth(result);
+    navigate(Path.Home);
   }
 
   const values = {
     loginSubmitHandler,
     registerSubmitHandler,
-    username: auth.username,
+    username: auth.username || auth.email,
     email: auth.email,
-    isAuthenticated: !!auth.username,
+    isAuthenticated: !!auth.email,
   }
 
   return (
