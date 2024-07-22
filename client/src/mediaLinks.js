fc7c4d07184950d9eb27f20ddef5e94c505c9358
Yoanna Popova <yoanna.dp@gmail.com>
2024-07-22 10:50:18 +0300

    Add media links into new file.

---
+++ client/src/mediaLinks.js
@@ -0,0 +1,7 @@
+const MediaLinks = {
+    Facebook: 'https://bg-bg.facebook.com/',
+    Instagram: 'https://www.instagram.com/',
+    Twitter: 'https://twitter.com/?lang=bg',
+}
+
+export default MediaLinks;
