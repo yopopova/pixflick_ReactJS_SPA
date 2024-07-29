8fbbdf72f4b5542cf2768dd377fe4c95ca836fc1
Yoanna Popova <yoanna.dp@gmail.com>
2024-07-29 09:36:46 +0300

    Filter and add current user profile movie records.

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -1,10 +1,25 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 
 import AuthContext from "../../contexts/authContext";
+import * as movieService from '../../services/movieService';
+import SingleMovie from '../all-movies/single-movie/SingleMovie';
 
 export default function Profile() {
-    const { username, avatar, email } = useContext(AuthContext);
-    // console.log(username);
+    const { userId, username, avatar, email } = useContext(AuthContext);
+    // console.log(userId);
+
+    const [movies, setMovies] = useState([]);
+    // console.log(movies);
+
+    useEffect(() => {
+        movieService.getUserMovies(userId)
+            .then(setMovies)
+            .catch(err => {
+                console.log(err);
+            });
+    }, []);
+
+    // console.log(movies);
 
     return (
         <main>
@@ -25,6 +40,20 @@ export default function Profile() {
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div className="all-movies">
+                <div className="movies">
+                    {movies.map(movie => (
+                        <SingleMovie key={movie._id} {...movie} />
+                    ))}
+                </div>
+
+                {/* {movies.length === 0 && (
+                <div className="no-movies">
+                    <p>No movies added yet...</p>
+                </div>
+                )} */}
             </div>
         </main>
     );
