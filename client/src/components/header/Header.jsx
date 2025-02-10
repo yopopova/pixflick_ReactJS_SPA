c000618a28f2066817a7d6eb14321b74254e86cc
Yoanna Popova <yoanna.dp@gmail.com>
2025-02-10 14:25:10 +0200

    Import CSS styles and set up Header component.

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 
+import headerStyles from './Header.module.css';
 import AuthContext from "../../contexts/authContext";
 
 export default function Header() {
@@ -10,29 +11,31 @@ export default function Header() {
     } = useContext(AuthContext);
 
     return (
-        <header className="header">
-            <div className="logo">
+        <header className={headerStyles.header}>
+            <div className={headerStyles.logo}>
                 <Link to="/">
-                    <span className="red">pix</span><span className="white">flick</span><span className="red">.</span>
+                    <span className={headerStyles.red}>pix</span>
+                    <span className={headerStyles.white}>flick</span>
+                    <span className={headerStyles.red}>.</span>
                 </Link>
             </div>
 
-            <nav className="nav">
+            <nav className={headerStyles.nav}>
                 <Link to="/">Home</Link>
                 <Link to="/movies">Movies</Link>
 
                 {!isAuthenticated && (
-                    <div id="guest">
+                    <div className={headerStyles.guest}>
                         <Link to="/register">Register</Link>
-                        <Link className="log" to="/login">Login</Link>
+                        <Link to="/login" className={headerStyles.log}>Login</Link>
                     </div>
                 )}
 
                 {isAuthenticated && (
-                    <div id="user">
+                    <div className={headerStyles.user}>
                         <Link to="/movies/create">Add Movie</Link>
                         <Link to="/profile">My Profile</Link>
-                        <Link className="log" to="/logout">Logout</Link>
+                        <Link to="/logout" className={headerStyles.log}>Logout</Link>
                     </div>
                 )}
             </nav>
