5f910ed78fec2b16d6b59a7ba3140d1d35c372a5
Yoanna Popova <yoanna.dp@gmail.com>
2025-02-13 18:46:10 +0200

    Import CSS styles and set up MovieDetails component.

--- client/src/components/movie-details/MovieDetails.jsx
+++ client/src/components/movie-details/MovieDetails.jsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect, useMemo, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
+import movieDetailsStyles from './MovieDetails.module.css';
+
 import * as movieService from '../../services/movieService';
 import * as commentService from '../../services/commentService';
 import AuthContext from "../../contexts/authContext";
@@ -64,68 +66,66 @@ export default function MovieDetails() {
     const isOwner = userId === movie._ownerId;
 
     return (
-        <main>
-            <section id="details">
-                <div id="banner">
-                    <img src={movie.wallpaper} alt={movie.title} />
-                </div>
+        <main className={movieDetailsStyles.details}>
+            <div className={movieDetailsStyles.banner}>
+                <img src={movie.wallpaper} alt={movie.title} />
+            </div>
 
-                <div className="movie">
-                    <div id="movie-info">
-                        <h1>{movie.title}</h1>
-                        <p><span>Genre: </span>{movie.genre}</p>
-                        <p><span>Year: </span>{movie.year}</p>
-                        <p><span>Description: </span>{movie.description}</p>
-
-                        {isOwner && (
-                            <div className="buttons">
-                                <Link to={pathToUrl(Path.Edit, { movieId })} className="button">Edit</Link>
-                                <button className="button" onClick={deleteButtonClickHandler}>Delete</button>
-                            </div>
-                        )}
-                    </div>
-
-                    <div id="movie-trailer">
-                        <iframe width="560" height="315"
-                        src={movie.trailer} 
-                        title="YouTube video player" frameBorder="0" 
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                        referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                    </div>
-                </div>
-
-                <div className="movie-comments">
-                    <h2>Comments</h2>
-
-                    <ul>
-                        {comments.map(({ _id, text, owner: { email } }) => (
-                            <li key={_id} className="comment">
-                                <p>{email}:</p>
-                                <p>{text}</p>
-                            </li>
-                        ))}
-                    </ul>
+            <div className={movieDetailsStyles.movie}>
+                <div className={movieDetailsStyles.info}>
+                    <h1>{movie.title}</h1>
+                    <p><span>Genre: </span>{movie.genre}</p>
+                    <p><span>Year: </span>{movie.year}</p>
+                    <p><span>Description: </span>{movie.description}</p>
                     
-                    {comments.length === 0 && (
-                        <p className="no-comments">There are no comments for this movie yet. Write the first one.</p>
+                    {isOwner && (
+                        <div className={movieDetailsStyles.buttons}>
+                            <Link className={movieDetailsStyles.button} to={pathToUrl(Path.Edit, { movieId })}>Edit</Link>
+                            <button className={movieDetailsStyles.button} onClick={deleteButtonClickHandler}>Delete</button>
+                        </div>
                     )}
                 </div>
 
-                <div className="create-comment">
-                    <h3>Like this movie? Share your impressions.</h3>
-
-                    {isAuthenticated && (
-                        <form id="add-comment" onSubmit={onSubmit}>
-                            <textarea name="comment" rows="10" cols="68" value={values.comment} onChange={onChange} placeholder="Add a comment..."></textarea>
-                            <input className="comment-btn" type="submit" value="Add Comment" />
-                        </form>
-                    )}
-                    
-                    {!isAuthenticated && (
-                        <Link className="details-login" to={Path.Login}>Login</Link>
-                    )}
+                <div className="trailer">
+                    <iframe width="560" height="315"
+                    src={movie.trailer} 
+                    title="YouTube video player" frameBorder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
                 </div>
-            </section>
+            </div>
+
+            <div className={movieDetailsStyles.comments}>
+                <h2>Comments</h2>
+
+                <ul>
+                    {comments.map(({ _id, text, owner: { email } }) => (
+                        <li key={_id} className="comment">
+                            <p>{email}:</p>
+                            <p>{text}</p>
+                        </li>
+                    ))}
+                </ul>
+                
+                {comments.length === 0 && (
+                    <p className={movieDetailsStyles.empty}>There are no comments for this movie yet. Write the first one.</p>
+                )}
+            </div>
+
+            <div className={movieDetailsStyles.create}>
+                <h3>Like this movie? Share your impressions.</h3>
+
+                {isAuthenticated && (
+                    <form className={movieDetailsStyles.add} onSubmit={onSubmit}>
+                        <textarea name="comment" rows="10" cols="68" value={values.comment} onChange={onChange} placeholder="Add a comment..."></textarea>
+                        <input className={movieDetailsStyles.btn} type="submit" value="Add Comment" />
+                    </form>
+                )}
+                
+                {!isAuthenticated && (
+                    <Link className={movieDetailsStyles.login} to={Path.Login}>Login</Link>
+                )}
+            </div>
         </main>
     );
 }
