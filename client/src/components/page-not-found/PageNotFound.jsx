a206d94318e2ad66fe6be13aaa50acf8e1750d9c
Yoanna Popova <yoanna.dp@gmail.com>
2025-02-18 11:48:39 +0200

    Import CSS styles and set up PageNotFound component.

--- client/src/components/page-not-found/PageNotFound.jsx
+++ client/src/components/page-not-found/PageNotFound.jsx
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
 
+import notFoundStyles from './PageNotFound.module.css';
+
 import Path from "../../paths";
 
 export default function PageNotFound() {
     return (
         <main>
-            <div id="error-page">
-                <div id="error-text">
+            <div className={notFoundStyles.page}>
+                <div className={notFoundStyles.text}>
                     <h1>404</h1>
                     <h2>Skadoosh... The page you are looking for does not exist!</h2>
 
-                    <div id="btns">
+                    <div className={notFoundStyles.btn}>
                         <Link to={Path.Home}>Home</Link>
                     </div>
                 </div>
